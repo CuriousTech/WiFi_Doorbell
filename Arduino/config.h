6401c7c50da01a055444bcc9d9d86f2f074c5f93
Greg Cunningham <greg@curioustech.net>
2021-07-21 03:11:15 -0400

    Create config.h

---
+++ Arduino/config.h
@@ -0,0 +1,5 @@
+// Configuration options
+
+#define OTA_ENABLE    // Uncomment to enable Arduino IDE Over The Air update code
+#define OLED_ENABLE   // Uncomment for OLED
+#define LEDRING_ENABLE // Uncomment for LED ring
