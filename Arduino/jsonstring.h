50eea7375dc60297cfb7db1ed7f511c24f06a3e7
Greg Cunningham <greg@curioustech.net>
2021-07-21 03:06:07 -0400

    Create jsonstring.h

---
+++ Arduino/jsonstring.h
@@ -0,0 +1,162 @@
+#include <ESPAsyncWebServer.h> // https://github.com/me-no-dev/ESPAsyncWebServer
+
+class jsonString
+{
+public:
+  jsonString(const char *pLabel = NULL)
+  {
+    m_cnt = 0;
+    if(pLabel)
+      s = pLabel, s += ";";
+    s += "{";
+  }
+        
+  String Close(void)
+  {
+    s += "}";
+    return s;
+  }
+
+  void Var(char *key, int iVal)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":";
+    s += iVal;
+    m_cnt++;
+  }
+
+  void Var(char *key, uint32_t iVal)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":";
+    s += iVal;
+    m_cnt++;
+  }
+
+  void Var(char *key, long int iVal)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":";
+    s += iVal;
+    m_cnt++;
+  }
+
+  void Var(char *key, float fVal)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":";
+    s += fVal;
+    m_cnt++;
+  }
+  
+  void Var(char *key, bool bVal)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":";
+    s += bVal ? 1:0;
+    m_cnt++;
+  }
+  
+  void Var(char *key, char *sVal)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":\"";
+    s += sVal;
+    s += "\"";
+    m_cnt++;
+  }
+  
+  void Var(char *key, String sVal)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":\"";
+    s += sVal;
+    s += "\"";
+    m_cnt++;
+  }
+
+  void Array(char *key, uint32_t iVal[], int n)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":[";
+    for(int i = 0; i < n; i++)
+    {
+      if(i) s += ",";
+      s += iVal[i];
+    }
+    s += "]";
+    m_cnt++;
+  }
+
+  void ArrayHdrs(char *key, AsyncWebServerRequest *request)
+  {
+    int nHeaders = request->headers();
+    if(nHeaders == 0) return;
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":[";
+
+    for(int i = 0; i < nHeaders; i++){
+      AsyncWebHeader* h = request->getHeader(i);
+      if(i) s += ",";
+      s +="\"";
+      s += h->name().c_str();
+      s += "=";
+      s += h->value().c_str();
+      s += "\"";
+    }
+    s += "]";
+    m_cnt++;
+  }
+
+  void ArrayPrms(char *key, AsyncWebServerRequest *request)
+  {
+    int nParams = request->params();
+    if(!nParams) return;
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":[";
+
+    for(int i = 0; i < nParams; i++)
+    {
+      AsyncWebParameter* p = request->getParam(i);
+      if(i) s += ",";
+      s += "\"";
+      if(p->isFile()){
+        s += "FILE";
+      } else if(p->isPost()){
+        s += "POST";
+      } else {
+        s += "GET";
+      }
+      s += ",";
+      s += p->name().c_str();
+      s += "=";
+      s += p->value().c_str();
+      s += "\"";
+    }
+    s += "]";
+  }
+
+protected:
+  String s;
+  int m_cnt;
+};
