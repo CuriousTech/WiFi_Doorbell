ef5626294f475c6ec5e5eea1570d2a78dc5c8cbb
Greg Cunningham <greg@curioustech.net>
2021-09-15 13:44:22 -0400

    Update LedRing.h

--- Arduino/LedRing.h
+++ Arduino/LedRing.h
@@ -32,13 +32,15 @@ public:
   void init(void);
   void service(void);
   void setEffect(uint8_t ef); // set an effect
-  void setColor(uint8_t r, uint8_t g, uint8_t b); // set color directly
+  void setColor(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness); // set color directly
   void light(uint8_t level); // set to white and return to last effect when set to 0
-  void alert(bool bOn);
+  void lightTimer(uint16_t sec);
+  void alert(uint16_t sec);
   void setIndicatorCount(uint8_t n); // number of LEDs for blink and chaser effects
   void setIndicatorType(int n);
   void Color(uint8_t n, uint8_t r, uint8_t g, uint8_t b);
-
+  void setBrightness(uint8_t n);
+  uint8_t m_brightness = 20;
 private:
   void glow(uint16_t j);
   void chaser(uint16_t n, uint8_t cnt);
@@ -50,11 +52,12 @@ private:
   void spiral(uint16_t n);
   void doubleSpiral(uint16_t n);
   void wings(uint16_t n);
+  void addGlitter(fract8 chanceOfGlitter);
 
   // AdaFruit examples
   void colorWipe(CRGB c, uint8_t wait);
   void rainbow(uint16_t j);
-  void rainbowCycle();
+  void rainbowCycle(void);
   CRGB Wheel(byte WheelPos);
 
   void setPixel(uint8_t i, CRGB c); // composite set color
@@ -63,6 +66,9 @@ private:
   uint8_t m_IndCnt;
   uint8_t m_IndType;
   uint8_t m_oldEffect = ef_Off;
+  uint8_t m_lightRevert;
+  uint16_t m_lightTimer;
+  uint16_t m_alertTimer;
   CRGB m_Color[4];
   CRGB m_leds[LED_CNT];
 
