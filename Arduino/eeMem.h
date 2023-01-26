6ada2ec67072a6349322fa1f2c53d36fbbcb03fd
Greg Cunningham <greg@curioustech.net>
2023-01-26 06:21:13 -0500

    Update eeMem.h

--- Arduino/eeMem.h
+++ Arduino/eeMem.h
@@ -3,29 +3,7 @@
 
 #include <Arduino.h>
 
-struct eeSet // EEPROM backed data
-{
-  uint16_t size;          // if size changes, use defauls
-  uint16_t sum;           // if sum is diiferent from memory struct, write
-  char    szSSID[32];
-  char    szSSIDPassword[64];
-  int8_t  tz;
-  char    location[32];   // location for weathermap
-  bool    bEnablePB[2];   // enable pushbullet for doorbell, motion
-  bool    bEnableOLED;
-  bool    bMotion;        // motion activated clear
-  char    pbToken[40];    // 34
-  char    owKey[36];      // 32
-  char    szNotifIP[16];
-  char    szNotifPath[44];
-  uint16_t NotifPort;
-  uint8_t  hostIP[4];
-  uint16_t hostPort;
-  uint8_t  melody;
-  uint8_t  effect;
-  uint8_t  bright[24];
-  uint8_t  res[14];
-};
+#define EESIZE (offsetof(eeMem, end) - offsetof(eeMem, size) )
 
 class eeMem
 {
@@ -34,9 +12,30 @@ public:
   void update(void);
 private:
   uint16_t Fletcher16( uint8_t* data, int count);
+public:
+  uint16_t size = EESIZE;          // if size changes, use defauls
+  uint16_t sum = 0xAAAA;           // if sum is diiferent from memory struct, write
+  char    szSSID[32] = "";
+  char    szSSIDPassword[64] = "";
+  int8_t  tz = -5;
+  char    location[32] =  "4291945";   // location for weathermap
+  bool    bEnablePB[2] = {false, false};   // enable pushbullet for doorbell, motion
+  bool    bEnableOLED = true;
+  bool    bMotion = false;        // motion activated clear
+  char    pbToken[40] = "pushbullet token";    // 34
+  char    owKey[36] = "openweather key";      // 32
+  char    szNotifIP[16] = "192.168.31.158"; // Another device that can make sounds or text
+  char    szNotifPath[44] = "/s?key=esp8266ct&music=0";
+  uint16_t NotifPort = 80;
+  uint8_t  hostIP[4] = {192,168,31,100};
+  uint16_t hostPort = 80;
+  uint8_t  melody = 0;
+  uint8_t  effect = 0;
+  uint8_t  bright[24] = {40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40,40};
+  uint8_t  res[14] = {0};
+  uint8_t end;
 };
 
-extern eeSet ee;
-extern eeMem eemem;
+extern eeMem ee;
 
 #endif // EEMEM_H
