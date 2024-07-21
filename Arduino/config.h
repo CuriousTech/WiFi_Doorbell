092c3e797fc9dcc1cc70064c7698eb461006bddf
Greg Cunningham <greg@curioustech.net>
2024-07-20 22:24:41 -0400

    Update config.h

--- Arduino/config.h
+++ Arduino/config.h
@@ -3,3 +3,4 @@
 #define OTA_ENABLE    // Uncomment to enable Arduino IDE Over The Air update code
 #define OLED_ENABLE   // Uncomment for OLED
 #define LEDRING_ENABLE // Uncomment for LED ring
+#define REOLINK // Uncomment for Reolink RF receiver (433MHz 3.3-5.0V)
